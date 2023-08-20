80953c362c68b353bf344765269df8704215d429
Razvan <tangent.jotey@gmail.com>
2023-08-20 15:11:40 +0200

    Adding proper ts support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path'
-import { fileURLToPath } from 'url'
-const file = fileURLToPath(import.meta.url)
-const dir = path.dirname(file).replace(/\\+/, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
