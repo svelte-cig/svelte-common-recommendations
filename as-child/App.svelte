7c17459b8fdc0402e40e8b36418e59a4e29fa822
Razvan <tangent.jotey@gmail.com>
2023-08-20 16:03:17 +0200

    formatting without tabs

--- as-child/App.svelte
+++ as-child/App.svelte
@@ -1,27 +1,27 @@
 <script>
-	import Tooltip from './Tooltip.svelte'
+  import Tooltip from './Tooltip.svelte'
 </script>
 
 <div class="grid gap-2 p-2">
-	<Tooltip let:Portal let:Trigger>
-		<Trigger>
-			<span>Click me</span>
-		</Trigger>
-		<Portal>
-			<span>thi is a tooltip content</span>
-		</Portal>
-	</Tooltip>
+  <Tooltip let:Portal let:Trigger>
+    <Trigger>
+      <span>Click me</span>
+    </Trigger>
+    <Portal>
+      <span>thi is a tooltip content</span>
+    </Portal>
+  </Tooltip>
 
-	<Tooltip let:Portal let:Trigger>
-		<Trigger>
-			<div slot="asChild" let:activate let:deactivate let:toggle>
-				<button type="button" class="btn-icon variant-filled" on:click={toggle}>
-					<span>+</span>
-				</button>
-			</div>
-		</Trigger>
-		<Portal>
-			<span>thi is a tooltip content</span>
-		</Portal>
-	</Tooltip>
+  <Tooltip let:Portal let:Trigger>
+    <Trigger>
+      <div slot="asChild" let:activate let:deactivate let:toggle>
+        <button type="button" class="btn-icon variant-filled" on:click={toggle}>
+          <span>+</span>
+        </button>
+      </div>
+    </Trigger>
+    <Portal>
+      <span>thi is a tooltip content</span>
+    </Portal>
+  </Tooltip>
 </div>
--- as-child/Portal.svelte
+++ as-child/Portal.svelte
@@ -1,27 +1,27 @@
 <style lang="scss">
-	.portal {
-		@apply absolute;
-		@apply bg-black;
-		@apply bg-opacity-70;
-		@apply p-4;
-		@apply rounded-xl;
-		@apply z-10;
-	}
+  .portal {
+    @apply absolute;
+    @apply bg-black;
+    @apply bg-opacity-70;
+    @apply p-4;
+    @apply rounded-xl;
+    @apply z-10;
+  }
 </style>
 
 <script>
-	import { getContext } from 'svelte'
-	import { fly } from 'svelte/transition'
-	/**
-	 * @type {import('svelte/store').Writable<{
-	 * 	visible: boolean
-	 * }>}
-	 */
-	const state = getContext('state')
+  import { getContext } from 'svelte'
+  import { fly } from 'svelte/transition'
+  /**
+   * @type {import('svelte/store').Writable<{
+   * 	visible: boolean
+   * }>}
+   */
+  const state = getContext('state')
 </script>
 
 {#if $state.visible}
-	<div class="portal" in:fly>
-		<slot />
-	</div>
+  <div class="portal" in:fly>
+    <slot />
+  </div>
 {/if}
